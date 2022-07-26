d56c4122a8a62f6ba0e08718ae0766ba8f5e6cd3
Tommy Nguyen <sixty22tommy@gmail.com>
2022-07-26 10:07:59 -0700

    done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,98 @@
-import React from "react";
+import React,{useState}  from "react";
 
 function Item({ name, category }) {
+
+  const [inCart, setCart] = useState(false)
+
+  const addItemToCart = ()=>{
+    setCart(true);
+    
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? 'in-cart': ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={addItemToCart} className="add">Add to Cart</button>
     </li>
   );
 }
 
 export default Item;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {useState} from "react";
+
+
+
+// function Item({ name, category }) {
+//   const [isInCart, setIsInCart] = useState(false)
+//   const classItem = isInCart ? "in-cart" : ""
+//   const classRemove = isInCart ? "in-cart" : "out-cart"
+
+//   function handleCartClick(){
+//     setIsInCart ((isInCart => !isInCart))
+//   }
+
+//   return (
+//     <li className={classItem}>
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button className="add"  onClick={handleCartClick}>Add to Cart</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+  
+
+  const handleFilterCategory = (e) =>{
+    setSelectedCategory(e.target.value);
+
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+      return true;
+    } else {
+      return item.category === selectedCategory;
+    }
+  }); 
+
+  const itemList = itemsToDisplay.map((item) => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  )); 
+  
+
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+      
+        <select name="filter" onChange={handleFilterCategory} >
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +38,97 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {itemList}
       </ul>
     </div>
   );
 }
 
+
+  
+
+
+  
 export default ShoppingList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, {useState} from "react";
+// import Item from "./Item";
+
+// function ShoppingList({ items }) {
+//   const [selectedCategory, setSelectedCategory] = useState("All")
+//   const [newItems] = useState(items)
+//   const itemsToDisplay = newItems.filter((items) => {
+//     if (selectedCategory === "All") {
+//       return true
+//     }
+//     else {
+//       return items.category === selectedCategory
+//     }
+//   })
+
+//   function handleChange(event){
+//     setSelectedCategory (event.target.value)
+//   }
+  
+
+
+//   return (
+//     <div className="ShoppingList">
+//       <div className="Filter">
+//         <select name="filter" onChange={handleChange}>
+//           <option value="All">Filter by category</option>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </div>
+//       <ul className="Items">
+//         {itemsToDisplay.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
